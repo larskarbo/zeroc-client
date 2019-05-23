2308860f0acb6942a8d71312ceeabbd11c8d486d
Lars Karbo <mail@larskarbo.no>
2019-05-23 15:20:38 +0200

    Updates

---
+++ precache-manifest.42c89591d2bfc4e40507fe23b5270bc7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9021f9788e6a425809ac2c98ee8ce9e8",
+    "url": "/zeroc-client/index.html"
+  },
+  {
+    "revision": "84f0bf4a2d1b4347ccad",
+    "url": "/zeroc-client/static/css/2.a56ffa26.chunk.css"
+  },
+  {
+    "revision": "2b7b8e5a2243fcd10963",
+    "url": "/zeroc-client/static/css/main.109a96cb.chunk.css"
+  },
+  {
+    "revision": "84f0bf4a2d1b4347ccad",
+    "url": "/zeroc-client/static/js/2.417a7e34.chunk.js"
+  },
+  {
+    "revision": "2b7b8e5a2243fcd10963",
+    "url": "/zeroc-client/static/js/main.d235f752.chunk.js"
+  },
+  {
+    "revision": "b84b328620e731eb299c",
+    "url": "/zeroc-client/static/js/runtime~main.9ec590c0.js"
+  },
+  {
+    "revision": "13de04a2fb94b75f1bb7fb01848ec0a2",
+    "url": "/zeroc-client/static/media/im.13de04a2.gif"
+  },
+  {
+    "revision": "dc6653448a617b0564541708101d3eac",
+    "url": "/zeroc-client/static/media/progr.dc665344.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "horse/precache-manifest.59f47cb99002935978fea2508254da10.js"
+  "/zeroc-client/precache-manifest.42c89591d2bfc4e40507fe23b5270bc7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("horse/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/zeroc-client/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
