1d3e0b1b6027aba3182eeda3679b3ed0c884bf1e
Lars Karbo <mail@larskarbo.no>
2019-05-23 15:02:33 +0200

    Updates

---
+++ precache-manifest.59f47cb99002935978fea2508254da10.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0a3742a2d8fe46fa49a758c62716ca4",
+    "url": "horse/index.html"
+  },
+  {
+    "revision": "84f0bf4a2d1b4347ccad",
+    "url": "horse/static/css/2.a56ffa26.chunk.css"
+  },
+  {
+    "revision": "2b7b8e5a2243fcd10963",
+    "url": "horse/static/css/main.109a96cb.chunk.css"
+  },
+  {
+    "revision": "84f0bf4a2d1b4347ccad",
+    "url": "horse/static/js/2.417a7e34.chunk.js"
+  },
+  {
+    "revision": "2b7b8e5a2243fcd10963",
+    "url": "horse/static/js/main.d235f752.chunk.js"
+  },
+  {
+    "revision": "990122c4889eed67545d",
+    "url": "horse/static/js/runtime~main.867591c3.js"
+  },
+  {
+    "revision": "13de04a2fb94b75f1bb7fb01848ec0a2",
+    "url": "horse/static/media/im.13de04a2.gif"
+  },
+  {
+    "revision": "dc6653448a617b0564541708101d3eac",
+    "url": "horse/static/media/progr.dc665344.gif"
+  }
+]);
